2ca58e59918a996324fc0da4906b0c55398bb106
Adithya K <adithya00394@gmail.com>
2025-06-22 15:14:04 +0530

    Initial commit with README.md

---
+++ src/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function About() {
+  return (
+    <main>
+      <h2>About Us</h2>
+      <p>StayHealthy helps you track and improve your wellness.</p>
+    </main>
+  );
+}
+
+export default About;
---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Welcome from '../components/Welcome';
+
+function Home() {
+  return (
+    <main>
+      <Welcome />
+      <p>This is the home page of your health app.</p>
+    </main>
+  );
+}
+
+export default Home;
